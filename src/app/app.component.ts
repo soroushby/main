94db13440c26e3b843b5ad3c6f860afc2b97071b
soroushby <49848393+soroushby@users.noreply.github.com>
2020-07-10 23:08:29 -0700

    started to style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mainMatab';
+
+  constructor(public auth: AuthService) {}
 }
